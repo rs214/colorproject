2fec15ddd45b6a62765ef780e55a32f1757d43fb
rs214 <robertsi0214@gmail.com>
2019-09-11 15:15:15 -0700

    create colorHelper function to generate color range

--- src/Palette.js
+++ src/Palette.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ColorBox from './ColorBox';
 import './Palette.css';
 
+
 export default class Palette extends Component {
   constructor(props) {
     super(props);
