c996ffca72a202d3d39137e5e8b9fb8c7f49832b
rs214 <robertsi0214@gmail.com>
2019-09-11 12:39:06 -0700

    seed colors for color project, add App and Palette components

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export default class Palette extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+      Palette
+      </div>
+    );
+  }
+}
