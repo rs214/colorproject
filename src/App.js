40c5ca75c1b2039eeb5f38862ee0755e76dc1905
rs214 <robertsi0214@gmail.com>
2019-09-13 15:08:23 -0700

    add id routes to palettes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
 class App extends Component {
+
+  findPalette(id) {
+    return seedColors.find(function(palette) {
+      return palette.id === id
+    });
+  }
+
   render() {
     return (
       <div>
         <Switch>
           <Route exact path="/" />
-          <Route exact path="/palette:id" />
+          <Route
+            exact path="/palette/:id"
+            render={routeProps => (
+              <Palette palette={
+                generatePalette(this.findPalette(routeProps.match.params.id))} />)}
+          />
         </Switch>
-        <div>
-          <Palette palette = {generatePalette(seedColors[4])}/>
-        </div>
       </div>
     );
   }
