530cd48fd7f6329b27ec2cb51f06fefb8c658eb2
Robert Si <robertsi0214@gmail.com>
2019-09-12 19:42:27 -0700

    add rc slider to project and integrate with Palette component

--- src/Palette.js
+++ src/Palette.js
@@ -1,20 +1,35 @@
 import React, {Component} from 'react';
 import ColorBox from './ColorBox';
 import './Palette.css';
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 
 export default class Palette extends Component {
   constructor(props) {
     super(props);
+    this.state = {level: 500}
+    this.changeLevel = this.changeLevel.bind(this);
   }
 
+  changeLevel(newLevel) {
+    this.setState({level: newLevel})
+  }
+
+
   render() {
-    console.log(this.props.palette.colors)
-    const colorBoxes = this.props.palette.colors[300].map(color => (
+    const colorBoxes = this.props.palette.colors[this.state.level].map(color => (
       <ColorBox background={color.hex} name={color.name} />
     ))
     return (
       <div className='Palette'>
+        <Slider
+          defaultValue={this.state.level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={this.changeLevel}
+        />
         <div className='Palette-colors'>
           {colorBoxes}
         </div>
