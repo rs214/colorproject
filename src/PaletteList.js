f5c279349c62f1524276873310e25569f32dd3e1
rs214 <robertsi0214@gmail.com>
2019-09-23 14:06:00 -0700

    add MiniPalette component using HOC component withStyles from material UI

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MiniPalette from './MiniPalette';
 import { Link } from 'react-router-dom';
 
 export default class PaletteList extends Component {
@@ -9,12 +10,13 @@ export default class PaletteList extends Component {
   render() {
     let palettesNames = this.props.palettes.map((palette) => {
       return (
-        <p>
-          <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>;
-        </p>
+        <MiniPalette {...palette}/>
       )})
     return (
-      <div>{palettesNames}</div>
+      <div>
+        <h1>PaletteList</h1>
+          {palettesNames}
+      </div>
     );
   }
 }
