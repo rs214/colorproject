93a4a5910e902745abda2bf439908895f6601562
rs214 <robertsi0214@gmail.com>
2019-09-27 12:24:45 -0700

    add single color palette and linked it to seemore in colorBox

---
+++ src/SingleColorPalette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class SingleColorPalette extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Single Color Palette</h1>
+      </div>
+    );
+  }
+}
